e4a7474f5288849c6ed9551ddfa1443d1e30b6f6
M.Venelinova <148716527+Miroslava-Venelinova@users.noreply.github.com>
2026-04-27 19:25:46 +0300

    Add files via upload
    exec of usps for players, admins, words for hangman

---
+++ SQLQuery_data.sql
@@ -0,0 +1,59 @@
+EXEC usp_AddPlayer 'Miroslava123!', '123M456', 0, 0, 0
+EXEC usp_AddPlayer 'Pepi1234', 'otntvanvn', 2, 2, 0
+EXEC usp_AddPlayer 'Zhoro', 'Zhorkoeqak', 13, 0, 5
+EXEC usp_AddPlayer 'Negreb2026','Negreb1232002', 4, 2, 0
+EXEC usp_AddPlayer 'Naiden1998', 'nfog123', 0, 0, 0
+EXEC usp_AddPlayer 'Player1', 'Player1', 12, 10, 0
+EXEC usp_AddPlayer 'GameSlayer', 'GetGames', 4, 1, 2
+EXEC usp_AddPlayer 'HiWorld', 'gameBeast23', 9, 0, 8
+EXEC usp_AddPlayer 'WordlEnthusiast', 'wordlislife', 14, 0, 13
+EXEC usp_AddPlayer 'HungUp', 'hangman2021', 5, 5, 0
+
+EXEC usp_AddAdmin '#A1Mikaela', 'admins1MI'
+EXEC usp_AddAdmin '#A2Petar', 'admins2PD'
+EXEC usp_AddAdmin '#A3Zhoro', 'admins3GP'
+EXEC usp_AddAdmin '#A4Martin', 'admins4MM'
+EXEC usp_AddAdmin '#A5Stoyan', 'admins5StN'
+
+EXEC usp_AddWordHangman 'щастлив', 'лесна'
+EXEC usp_AddWordHangman 'такси', 'лесна'
+EXEC usp_AddWordHangman 'котка', 'лесна'
+EXEC usp_AddWordHangman 'стол', 'лесна'
+EXEC usp_AddWordHangman 'спокоен', 'средна'
+EXEC usp_AddWordHangman 'шумен', 'средна'
+EXEC usp_AddWordHangman 'куче', 'лесна'
+EXEC usp_AddWordHangman 'маса', 'лесна'
+EXEC usp_AddWordHangman 'мръсен', 'сложна'
+EXEC usp_AddWordHangman 'любопитен', 'сложна'
+EXEC usp_AddWordHangman 'свеж', 'лесна'
+EXEC usp_AddWordHangman 'млад', 'средна'
+EXEC usp_AddWordHangman 'тъжен', 'средна'
+EXEC usp_AddWordHangman 'велосипед', 'сложна'
+EXEC usp_AddWordHangman 'нов', 'лесна'
+EXEC usp_AddWordHangman 'нервен', 'сложна'
+EXEC usp_AddWordHangman 'безразличен', 'сложна'
+EXEC usp_AddWordHangman 'корем', 'средна'
+EXEC usp_AddWordHangman 'влак', 'лесна'
+EXEC usp_AddWordHangman 'площад', 'средна'
+EXEC usp_AddWordHangman 'слънце', 'средна'
+EXEC usp_AddWordHangman 'грозен', 'сложна'
+EXEC usp_AddWordHangman 'дъжд', 'лесна'
+EXEC usp_AddWordHangman 'търпелив', 'средна'
+EXEC usp_AddWordHangman 'гръб', 'лесна'
+EXEC usp_AddWordHangman 'мотоциклет', 'сложна'
+EXEC usp_AddWordHangman 'земя', 'лесна'
+EXEC usp_AddWordHangman 'врат', 'лесна'
+EXEC usp_AddWordHangman 'небрежен', 'сложна'
+EXEC usp_AddWordHangman 'пазар', 'лесна'
+EXEC usp_AddWordHangman 'дим', 'лесна'
+EXEC usp_AddWordHangman 'кораб', 'лесна'
+EXEC usp_AddWordHangman 'внимателен', 'средна'
+EXEC usp_AddWordHangman 'неточен', 'сложна'
+EXEC usp_AddWordHangman 'цвете', 'лесна'
+EXEC usp_AddWordHangman 'празен', 'лесна'
+EXEC usp_AddWordHangman 'плаж', 'лесна'
+EXEC usp_AddWordHangman 'книга', 'лесна'
+EXEC usp_AddWordHangman 'прост', 'средна'
+EXEC usp_AddWordHangman 'ходя', 'лесна'
+
+
